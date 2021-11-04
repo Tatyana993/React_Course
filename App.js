9a2f6a7c89b57865267e22f02408b3428659ac47
Tatyana993 <84542378+Tatyana993@users.noreply.github.com>
2021-11-04 19:56:03 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Message } from "./components/Message/Message.jsx";
 import { Form } from "./components/Form";
 import { MessageList } from "./components/MessageList";
 import { useState, useEffect, useCallback } from "react";
-
-let message = "Hello, my first project";
 
 const userMessages = [
   {
@@ -24,13 +20,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Message text={message} />
-        </p>
         <div>
-          <Form SendMessage={sendMessages} />
           <MessageList messages={messages} />
+          <Form sendMessages={sendMessages} />
         </div>
       </header>
     </div>
