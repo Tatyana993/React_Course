f0f8c467d9dc681c4d38bdfbdb47d5f77496d032
Tatyana993 <84542378+Tatyana993@users.noreply.github.com>
2021-11-08 19:21:41 +0300

    DZ-3

--- Form.jsx
+++ Form.jsx
@@ -1,28 +1,46 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
+import { AUTHOR } from "../utils/constants";
 
-export const Form = ({}) => {
+
+import { InputGroup } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import Button from 'react-bootstrap/Button';
+
+
+export const Form = ({sendMessages}) => {
   const[value, setValue] = useState('');
+  const inputRef = useRef();
 
   const handleChange = (e) => {
     setValue(e.target.value);
-    console.log(value);
-  
+    
   }
-
-
 
 const handleSubmit = (e) => {
     e.preventDefault();
+    sendMessages({
+      text:value,
+      author: AUTHOR.human,
+      id: `mes-${Date.now()}`
+    });
+
+    inputRef.current?.autofocus();
+    setValue('')
   
   }
-
-
   return(
-    <form onSubmit={handleSubmit}>
-      <input type="text" value={value} onChange={handleChange}/>
-      <button>Отправить</button>
-      <p></p>
-   </form>
-     
+<form onSubmit={handleSubmit}>
+   <InputGroup className="mb-3">
+    <FormControl
+      aria-label="Recipient's username"
+      aria-describedby="basic-addon2"
+      value={value} onChange={handleChange}
+      inputRef={inputRef}
+    />
+    <Button variant="secondary" id="button-addon2" type="submit" size="lg">
+      SEND
+    </Button>
+  </InputGroup>
+   </form>  
   )
 }
