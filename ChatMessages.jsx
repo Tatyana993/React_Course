c1cdef74add3fb2fe02de9d9085f8d21921ada65
Tatyana993 <84542378+Tatyana993@users.noreply.github.com>
2021-11-14 22:59:41 +0300

    DZ-4

--- ChatMessages.jsx
+++ ChatMessages.jsx
@@ -1,37 +1,28 @@
 import React from "react";
 
-import Button from 'react-bootstrap/Button';
-import { Popover } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
 import { OverlayTrigger } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const chatMessseng = {
-    name: "Ivan",
-    id: Date.now(),
-  };
+const chatMessseng = [
+  { name: "chat1", id: "chat1" },
+  { name: "chat2", id: "chat2" },
+  { name: "chat3", id: "chat3" },
+];
 
 export const ChatMessages = () => {
-  
-
-const popover = (
-  <Popover id="popover-basic" >
-    <Popover.Header as="h3">{chatMessseng.name}</Popover.Header>
-    <Popover.Body>
-    {chatMessseng.id}
-    </Popover.Body>
-  </Popover>
-);
-
-const Example = () => (
-  <OverlayTrigger trigger="click" placement="right" overlay={popover}>
-    <div className="d-grid gap-2">
-    <Button variant="success" >Chat1</Button>
-    <Button variant="warning" >Chat2</Button>
-    <Button variant="secondary" >Chat3</Button>
-     </div>
-  </OverlayTrigger>
-);
-
-return(<Example />
-      
+  const Example = () => (
+    <OverlayTrigger trigger="click" placement="right">
+      <div className="d-grid gap-2">
+        {chatMessseng.map((chat) => (
+          <Link to={`/chats/${chat.id}`}>
+            {chat.name}
+            <Button variant="success">Send</Button>
+          </Link>
+        ))}
+      </div>
+    </OverlayTrigger>
   );
-}
+
+  return <Example />;
+};
